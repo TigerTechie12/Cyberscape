d1b535412bafa6132d7e157ce41acbdab793cce8
Shreyash <shreyashiitr22@gmail.com>
2026-01-03 22:45:07 +0530

    authentication tests completed

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const BACKEND_URL=""
 
 describe('Authentication',()=>{
-test('User is able to sign up',async()=>{
+test('Admin is able to sign up only once',async()=>{
 const username="testuser"+ Math.random()
 const password="testpassword"
 const response=await axios.post(`${BACKEND_URL}/api/v1/user/signup`,{
@@ -11,6 +11,48 @@ const response=await axios.post(`${BACKEND_URL}/api/v1/user/signup`,{
     type:'admin'
 })
 expect(response.statusCode).toBe(200)
+
+const updatedResponse=await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+    username:username,  
+    password:password,
+    type:'admin'
+})
+expect(updatedResponse.statusCode).toBe(400)
+})
+test('Signup request fails if the username is empty',async()=>{
+    const username=`testuser`+ Math.random()
+    const password="testpassword"
+    const response= await axios.post(`${BACKEND_URL}/api/v1/user/signup`,{
+        username:username
+    })
+    expect(response.statusCode).toBe(400)
+})
+test('Signin succeeds if the username and password are correct',async()=>{
+    const username=`testuser` + Math.random()
+    const password="testpassword"
+    await axios.post(`${BACKEND_URL}/api/v1/user/signup`,{
+        username:username,
+        password:password,
+})
+const response=await axios.post(`${BACKEND_URL}/api/v1/signin`,{
+    username:username,
+    password:password,
+})
+expect(response.statusCode).toBe(200)
+expect(response.data.token).toBeDefined()
+})
+test('Signin fails if the username and password are incorrect',async()=>{
+const username="testuser"+ Math.random()
+const password="testpassword"
+await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+    username:username,
+    password:password,
+})
+const response=await axios.post(`${BACKEND_URL}/api/v1/signin`,{
+    username:"shjioshds",
+    password:password
+})
+expect(response.statusCode).toBe(403)
+})
 })
 
-})
