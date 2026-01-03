5e4f15dc6844c4c417d4ae00320db1506329017e
Shreyash <shreyashiitr22@gmail.com>
2026-01-04 04:05:39 +0530

    test for user meta data endpoint

--- tests/index.test.js
+++ tests/index.test.js
@@ -56,3 +56,48 @@ expect(response.statusCode).toBe(403)
 })
 })
 
+describe('User Information Endpoints',()=>{
+    let token=""
+    let avatarId=""
+  beforeAll(async()=>{
+    const username=`test+${Math.random()}`
+    const password="testpassword"
+    await axios.post(`${BACKEND_URL}/api/v1/user/signup`,{
+        username:username,
+        password:password,
+        type:'admin'
+    })
+  const response=  await axios.post(`${BACKEND_URL}/api/v1/signin`,{
+username:username,
+password:password
+    })
+token=response.data.token
+const avatarResponse=await axios.post(`${BACKEND_URL}/api/v1/admin/avatar`,{
+    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+	"name": "Timmy"
+})
+avatarId=response.data.avatarId
+  })
+  test('User cant update their metadata with a wrong avatar id',async()=>{
+const response=await axios.post(`${BACKEND_URL}/api/v1/user/metadata`,{
+    avatarId:"32242234"
+},{headers:{"authorization":`Bearer ${token}`}})
+expect(response.statusCode).toBe(400)
+  })
+
+  test("User can update their metadata with correct avatar id",async()=>{
+    const response=await axios.post(`${BACKEND_URL}/api/v1/user/metadata`,{
+        avatarId:avatarId
+    },{
+        headers:{"authorization":`Bearer ${token}`}
+    })
+  expect(response.statusCode).toBe(200)
+})
+test("User forgot to send the headers in the request",async()=>{
+    const response=await axios.post(`${BACKEND_URL}/api/v1/user/metadata`,{
+        avatarId:avatarId
+    })
+    except(response.statusCode).toBe(403)
+})
+
+})
