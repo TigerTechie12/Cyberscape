13933e10d3dbc3cf6dd1511590419dae01c21c9c
Shreyash <shreyashiitr22@gmail.com>
2026-01-13 18:01:00 +0530

    db changes

---
+++ packages/db/dist/generated/prisma/browser.d.ts
@@ -0,0 +1,40 @@
+import * as Prisma from './internal/prismaNamespaceBrowser.js';
+export { Prisma };
+export * as $Enums from './enums.js';
+export * from './enums.js';
+/**
+ * Model User
+ *
+ */
+export type User = Prisma.UserModel;
+/**
+ * Model Avatar
+ *
+ */
+export type Avatar = Prisma.AvatarModel;
+/**
+ * Model mapElements
+ *
+ */
+export type mapElements = Prisma.mapElementsModel;
+/**
+ * Model Space
+ *
+ */
+export type Space = Prisma.SpaceModel;
+/**
+ * Model spaceElements
+ *
+ */
+export type spaceElements = Prisma.spaceElementsModel;
+/**
+ * Model Element
+ *
+ */
+export type Element = Prisma.ElementModel;
+/**
+ * Model Maps
+ *
+ */
+export type Maps = Prisma.MapsModel;
+//# sourceMappingURL=browser.d.ts.map
