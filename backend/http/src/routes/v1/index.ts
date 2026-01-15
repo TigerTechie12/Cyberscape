fd09f4237b1a76dca1d4b1ef9482f43685157e5f
Shreyash <shreyashiitr22@gmail.com>
2026-01-15 08:02:43 +0530

    db migrations and changes in types and user signin and signup logic added

--- backend/http/src/routes/v1/index.ts
+++ backend/http/src/routes/v1/index.ts
@@ -1,28 +1,54 @@
 import { Router } from "express";
 import { InputModel } from "common";
 import {client} from "@repo/db/client"
+import jwt from 'jsonwebtoken'
 const router=Router()
+const JWT_SECRET:any=process.env.JWT_SECRET
 router.post('/signup',async(req,res)=>{
 const inputs=req.body
 const parsedData=InputModel.safeParse(inputs)
 if(!parsedData.success){
     return res.status(400)
 }
-const userData= await client.user.create({data:
+try{const userData:any= await client.user.create({data:
     {username:parsedData.data.username,
 password:parsedData.data.password,
 type:parsedData.data.type
 }
 
 })
-return res.json({message:"Signup"}).status(200)
+const userId=userData.data.id
+return res.json({userId}).status(200)}
+catch(e){
+    res.status(403).json({message:"something went wrong"})
+}
 })
 
-router.post('/signin',(req,res)=>{
+router.post('/signin',async(req,res)=>{
 const inputs=req.body
-if(!InputModel.safeParse(inputs).success){
+const parsedData=InputModel.safeParse(inputs)
+if(!parsedData.success){
     return res.status(400)
 }
-return res.json({message:"Signin"}).status(200)
+try{const dbFindUser=await client.user.findUnique({where:{username:parsedData.data.username,
+    password:parsedData.data.password
+}})
+if(dbFindUser){
+    const token=jwt.sign({username:parsedData.data.username,
+      
+        type:parsedData.data.type
+    },
+JWT_SECRET,{
+    expiresIn:"24h"
+})
+return res.json({token}).status(200)
+}
+else{
+    return res.status(400).json({message:"User not found"})
+}
 
+}
+catch(e){
+    return res.status(403).json({message:"something went wrong"})
+}
 })
