a0bdf023d1038db7fffab56cc013b1367bf3debd
Shreyash <shreyashiitr22@gmail.com>
2026-01-15 20:17:05 +0530

    db integration for user's api

--- backend/http/src/routes/v1/user.ts
+++ backend/http/src/routes/v1/user.ts
@@ -1,24 +1,60 @@
 import { userMetaData } from "common";
 import { Router } from "express";
-
+import {client} from '@repo/db/client'
+import jwt,{type JwtPayload}  from "jsonwebtoken";
 const router=Router()
-router.post('/metadata',(req,res)=>{
+const JWT_SECRET:any=process.env.JWT_SECRET
+router.post('/metadata',async(req,res)=>{
     const body=req.body
-    if(!userMetaData.safeParse(body)){
+    const parsedData:any=userMetaData.safeParse(body)
+    if(!parsedData.success){
         res.status(400).json({message:"Wrong Inputs"})
     }
-    //Id user doesnt exist in db return 403
-res.status(200).json({Message:"Udpated metadata"})
+   
+  
+    try{  const authHeader=req.headers.authorization
+    const token:any=authHeader?.split("")[1] 
+        const decoded=jwt.verify(token,JWT_SECRET) as JwtPayload
+        const id:any=decoded.userId 
+        const updateData=await client.user.update({where:{id:id},
+            data:{
+                avatarId:parsedData.data.avatarId
+            }})
+        res.status(200).json({Message:"Udpated metadata"})
+    }
 
+catch(e){
+    res.status(403).json({message:"Something went wrong"})
+}
 })
-router.get('/avatars',(req,res)=>{
-//get info from db and return
-res.status(200).json({})
+router.get('/avatars',async(req,res)=>{
+
+
+try{
+    const authHeader=req.headers.authorization
+    const token:any=authHeader?.split("")[1]
+    const decoded=jwt.verify(token,JWT_SECRET) as JwtPayload
+    const id:any=decoded.userId
+const avatarId:any=await client.user.findMany({where:{id:id},
+    select:{avatarId:true}
+})
+
+    const avatars=await client.avatar.findMany({
+where:{id:avatarId.data.avatarId},
+select:{id:true,
+    name:true,
+    imageUrl:true
+}
+    })
+    res.status(200).json({avatars})}
+catch(e){res.status(403).json({message:"Something went wrong"})}
 
 })
 
 router.get('/metadata/bulk?ids=[]',(req,res)=>{
     //get info from db and return
+    
+    
 res.status(200).json({})
 })
 
