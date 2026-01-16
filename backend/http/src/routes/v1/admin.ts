a4e1a8079ab725aba9758df7e802ce275179e1f0
Shreyash <shreyashiitr22@gmail.com>
2026-01-16 19:25:43 +0530

    changes in backend routes

--- backend/http/src/routes/v1/admin.ts
+++ backend/http/src/routes/v1/admin.ts
@@ -3,23 +3,45 @@ import { mapCreator,updateImage,avatarInputs,adminMap } from "common";
 import {client} from '@repo/db/client'
 const router=Router()
 
-router.post('/admin/element',(req,res)=>{
+router.post('/admin/element',async(req,res)=>{
     const body=req.body
-if(!mapCreator.safeParse(body).success){
-    res.json({message:"Wrong Inputs"})
-  
+    const parsedData=mapCreator.safeParse(body)
+if(!parsedData.success){
+  return  res.json({message:"Wrong Inputs"})
+}
+try{const dbData=await client.element.create({
+    data:{
+        imageUrl:parsedData.data.imageUrl,
+        width:parsedData.data.width,
+        height:parsedData.data.height,
+        static:parsedData.data.static
+    }
+})
+const id=dbData.id
+return res.status(200).json({id})
+}
+catch(e){
+    return res.status(403).json({
+        message:"Something went wrong"
+    })
 }
 
-res.json({id})
 })
-router.put('/admin/element/:elementId',(req,res)=>{
+router.put('/admin/element/:elementId',async(req,res)=>{
     const id=req.params
     const body=req.body
-if(!updateImage.safeParse(body).success){
-        res.json({message:"Wrong Inputs"})
+    const parsedData=updateImage.safeParse(body)
+if(!parsedData.success){
+       return res.json({message:"Wrong Inputs"})
 }
-//db logic
-res.status(200).json({message:"Inputs updated"})
+try{
+    const dbData=
+    
+    res.status(200).json({message:"Inputs updated"})
+
+}
+catch(e){}
+
 })
 router.post('/admin/avatar',(req,res)=>{
 const body=req.body
