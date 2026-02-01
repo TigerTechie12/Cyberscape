b67fd358960896370584909970ca581ffd3a408a
Shreyash <shreyashiitr22@gmail.com>
2026-02-01 21:58:41 +0530

    changes for deployment

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
       <Route element={<ProtectedRoute userOnly />}>
